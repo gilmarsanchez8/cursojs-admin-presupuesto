a27769876f3206d490672a385488bc918b52ff06
Gilmar Sanchez Cuartas <gilmarsancu@gmail.com>
2020-11-01 11:23:53 -0500

    Validación de campos y listar los gastos agregados

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ const gastoListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners(){
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+
+    formulario.addEventListener('submit', agregarGasto);
 }
 
 //Classes
@@ -14,12 +16,74 @@ class Presupuesto{
     constructor(presupuesto){
         this.presupuesto = Number(presupuesto);
         this.restante = Number(presupuesto);
-        this.gasto = [];
+        this.gastos = [];
+    }
+
+    nuevoGasto(gasto){
+        this.gastos = [...this.gastos, gasto];
+        //console.log(this.gastos);
     }
 }
 
 class UI{
+    insertarPresupuesto(cantidad){
+        const {presupuesto, restante} = cantidad;
+        document.querySelector('#total').textContent = presupuesto;
+        document.querySelector('#restante').textContent = restante;
+    }
 
+    imprimirAlerta(mensaje, tipo){
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+
+        if(tipo === 'error'){
+            divMensaje.classList.add('alert-danger');
+        }else{
+            divMensaje.classList.add('alert-success');
+        }
+
+        divMensaje.textContent = mensaje;
+
+        document.querySelector('.primario').insertBefore(divMensaje,formulario);
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
+
+    agregarGastoListado(gastos){
+
+        this.limpiarHTML(); //Elimina el HTML previo
+        //Iterar sobre los gastos
+        gastos.forEach(gasto => {
+            const {cantidad, nombre, id} = gasto;
+
+            //Crear un li
+            const nuevoGasto = document.createElement('li');
+            nuevoGasto.className = 'list-group-item d-flex justify-content-between align-items-center';
+            nuevoGasto.dataset.id = id;
+
+            //Agregar el HTML del gasto
+            nuevoGasto.innerHTML = `${nombre} <span class="badge badge-primary badge-pill"> ${cantidad} </span>`;
+            
+            //Boton para borrar el registro
+            const btnBorrar = document.createElement('button');
+            btnBorrar.classList.add('btn', 'btn-danger', 'borrar-gasto');
+            btnBorrar.innerHTML = 'Borrar &times;';
+            nuevoGasto.appendChild(btnBorrar);
+
+            //Agregar al HTML
+            gastoListado.appendChild(nuevoGasto);
+
+        });
+    }
+
+    limpiarHTML(){
+
+        while(gastoListado.firstChild){
+            gastoListado.removeChild(gastoListado.firstChild);
+        }
+    }
 }
 
 //Instanciar
@@ -35,5 +99,38 @@ function preguntarPresupuesto(){
     }
 
     presupuesto = new Presupuesto(presupuestoUsuario);
-    console.log(presupuesto);
+    //console.log(presupuesto);
+    ui.insertarPresupuesto(presupuesto);
+}
+
+function agregarGasto(e){
+    e.preventDefault();
+
+    //Leer datos del formulario
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = Number(document.querySelector('#cantidad').value);
+
+    if(nombre === '' || cantidad === ''){
+        ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
+        return;
+    }else if(cantidad <= 0 || isNaN(cantidad)){
+        ui.imprimirAlerta('Cantidad inválida', 'error');
+        return;
+    }
+
+    //Generar objeto con el gasto
+    const gasto = {nombre, cantidad, id: Date.now()}
+
+    //Añadir un nuevo gasto
+    presupuesto.nuevoGasto(gasto);
+
+    //Mensaje de éxito
+    ui.imprimirAlerta('Gasto agregado correctamente');
+
+    //Imprimir los gatos
+    const {gastos} = presupuesto;
+    ui.agregarGastoListado(gastos);
+
+    //Reinicia el formulario
+    formulario.reset();
 }
